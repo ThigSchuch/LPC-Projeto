8b1b63b00ea14ad239279e28590e2d058f5bd27e
ThigSchuch <thigschuch@gmail.com>
2019-03-08 21:19:09 -0300

    Noticias + coisas

--- app_noticias/models.py
+++ app_noticias/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Noticia(models.Model):
+
+    class Meta:
+        verbose_name = 'Notícias'
+        verbose_name_plural = 'Notícias'
+
+    titulo = models.CharField('Título', max_length=128)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField()
+    data_publicacao = models.DateTimeField()
+    publicado = models.BooleanField()
